6aa555dcb66844fcb37e8563f24c0f8d098a54a6
Ryan Park <98303656+RyanJPark03@users.noreply.github.com>
2025-07-08 12:12:27 -0500

    started decoupling

--- exp/hockey/Hockey.jl
+++ exp/hockey/Hockey.jl
@@ -173,11 +173,11 @@ goal_position = [
 function f(xs::BlockVector, us::BlockVector, ms::BlockVector)
     BlockVector(
             mapreduce(vcat, zip(xs.blocks, us.blocks, ms.blocks)) do (xᵢ, uᵢ, mᵢ)
-            [1 0 dt 0; 0 1 0 dt; 0 0 1 0; 0 0 0 1] * xᵢ +
-            [0.5*dt^2 0; 0 0.5*dt^2; dt 0; 0 dt] * uᵢ +
-            [0.01 0 0 0; 0 0.01 0 0; 0 0 0.01 0; 0 0 0 0.01] * mᵢ
+            [1 0; 0 1] * xᵢ +
+            [1 0; 0 1] * uᵢ +
+            [0.1 0; 0 0.1] * mᵢ
         end,
-        [4, 4]
+        [2, 2]
     )
 end
 
@@ -185,18 +185,16 @@ end
 function h(xs::BlockVector, ns::BlockVector)
     BlockVector(
         mapreduce(vcat, zip(xs.blocks, ns.blocks)) do (xᵢ, nᵢ)
-            [1 0 0 0; 0 1 0 0] * xᵢ + [0.01 0 0 0; 0 0.01 0 0] * nᵢ
+            [1 0; 0 1] * xᵢ + [0.1 0; 0 0.1] * nᵢ
         end,
         [2, 2]
     )
 end
 # Cost
-function steal_liklihood(bs::Beliefs)
-    attacker_belief = bs.beliefs[1]
-    defender_belief = bs.beliefs[2]
+function steal_liklihood(belief_over_attacker::Belief, belief_over_defender::Belief)
 
-    attacker_pos = attacker_belief.belief_mean[1:2]
-    defender_pos = defender_belief.belief_mean[1:2]
+    attacker_pos = belief_over_attacker.belief_mean
+    defender_pos = belief_over_defender.belief_mean
 
     sq_dist = dot(attacker_pos - defender_pos, attacker_pos - defender_pos)
 
@@ -212,25 +210,25 @@ function steal_liklihood(bs::Beliefs)
     
     geometric_bonus = blocking_factor * proximity_factor
     
-    attacker_pos_uncertainty = tr(attacker_belief.belief_covariance[1:2, 1:2])
-    defender_pos_uncertainty = tr(defender_belief.belief_covariance[1:2, 1:2])
+    attacker_pos_uncertainty = tr(belief_over_attacker.belief_covariance)
+    defender_pos_uncertainty = tr(belief_over_defender.belief_covariance)
     total_pos_uncertainty = attacker_pos_uncertainty + defender_pos_uncertainty
     
     return 5.0 * geometric_bonus - 0.5 * total_pos_uncertainty
 end
 function defender_non_terminal_cost(bs::Beliefs, us)
-    steal_prob = steal_liklihood(bs)
+    steal_prob = steal_liklihood(bs.beliefs[3], bs.beliefs[4])
     control_effort = dot(us[Block(1)], us[Block(1)])
-    return -2 * steal_prob + 2 * control_effort + dot(bs.beliefs[2].belief_mean[1:2], bs.beliefs[2].belief_mean[1:2])^2
+    return -2 * steal_prob + 2 * control_effort + dot(bs.beliefs[4].belief_mean, bs.beliefs[4].belief_mean)^2
 end
 function attacker_non_terminal_cost(bs::Beliefs, us)
-    steal_prob = steal_liklihood(bs)
+    steal_prob = steal_liklihood(bs.beliefs[1], bs.beliefs[2])
     goal_center = (goal_position[1] + goal_position[2]) / 2
-    dist_to_goal_sq = dot(bs.beliefs[1].belief_mean[1:2] - goal_center, bs.beliefs[1].belief_mean[1:2] - goal_center)
+    dist_to_goal_sq = dot(bs.beliefs[1].belief_mean - goal_center, bs.beliefs[1].belief_mean - goal_center)
     control_effort = dot(us[Block(2)], us[Block(2)])
-    return 1 * steal_prob + 4 * control_effort + dot(bs.beliefs[1].belief_mean[1:2], bs.beliefs[1].belief_mean[1:2])^2 + 0.1 * dist_to_goal_sq
+    return 1 * steal_prob + 4 * control_effort + dot(bs.beliefs[1].belief_mean, bs.beliefs[1].belief_mean)^2 + 0.1 * dist_to_goal_sq
 end    
-function shot_probability(bs::Beliefs)
+function shot_probability(belief_over_attacker::Belief, belief_over_defender::Belief)
     dist_penalty = 0.1
     block_max = 3
     block_falloff = 0.8
@@ -238,10 +236,10 @@ function shot_probability(bs::Beliefs)
     defender_uncertainty_penalty = 0.5 
     goal_center = (goal_position[1] + goal_position[2]) / 2
 
-    attacker_pos = bs.beliefs[1].belief_mean[1:2] # Player 1 is Attacker
-    defender_pos = bs.beliefs[2].belief_mean[1:2] # Player 2 is Defender
-    attacker_pos_uncertainty = tr(bs.beliefs[1].belief_covariance[1:2, 1:2])
-    defender_pos_uncertainty = tr(bs.beliefs[2].belief_covariance[1:2, 1:2])
+    attacker_pos = belief_over_attacker.belief_mean # Player 1 is Attacker
+    defender_pos = belief_over_defender.belief_mean # Player 2 is Defender
+    attacker_pos_uncertainty = tr(belief_over_attacker.belief_covariance)
+    defender_pos_uncertainty = tr(belief_over_defender.belief_covariance)
 
     # Term 1: Base score, penalized by distance to goal and attacker's own uncertainty.
     dist_sq_to_goal = dot(attacker_pos - goal_center, attacker_pos - goal_center)
@@ -271,10 +269,10 @@ function attacker_terminal_cost(bs::Beliefs)
     # Attacker wants to max shot quality, so we min its negative.
     # Don't let attacker get too far away from origin (area of play). This game construction
     #   doesn't allow for hard constraints.
-    return -10 * shot_probability(bs)
+    return -10 * shot_probability(bs.beliefs[1], bs.beliefs[2])
 end
 function defender_terminal_cost(bs::Beliefs)
-    return 10 * shot_probability(bs)
+    return 10 * shot_probability(bs.beliefs[3], bs.beliefs[4])
 end
 function nature_non_terminal_cost(bs::Beliefs, us::BlockVector)
     steal_prob = dot(bs.beliefs[1].belief_mean[1:2] - bs.beliefs[2].belief_mean[1:2], bs.beliefs[1].belief_mean[1:2] - bs.beliefs[2].belief_mean[1:2])
@@ -380,14 +378,14 @@ function receding_horizon_main(file_num=1; horizon=20, plotting_horizon=10, over
     end
 
     gt_initial_state = mortar([
-        [0.75, 5.0, 0.0, 0.0],  # Attacker
-        [-0.75, 1.5, 0.0, 0.0], # Defender
+        [0.75, 5.0],  # Attacker
+        [-0.75, 1.5], # Defender
     ])
     initial_belief_covariance = [
-        [0.1 0 0 0; 0 0.1 0 0; 0 0 0.25 0; 0 0 0 0.25],
-        [0.1 0 0 0; 0 0.1 0 0; 0 0 0.25 0; 0 0 0 0.25],
+        [0.1 0; 0 0.1],
+        [0.1 0; 0 0.1],
     ]
-    initial_beliefs = Beliefs([Belief(gt_initial_state[Block(i)], initial_belief_covariance[i]) for i in 1:2])
+    initial_beliefs = Beliefs([Belief(gt_initial_state[Block(i % 2 + 1)], initial_belief_covariance[i % 2 + 1]) for i in 1:4])
 
     attacker_cost = BeliefCost(attacker_non_terminal_cost, attacker_terminal_cost)
     defender_cost = BeliefCost(defender_non_terminal_cost, defender_terminal_cost)
