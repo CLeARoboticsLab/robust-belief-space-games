ab66d951636090af9add6fdcf68390a322fcf445
Ryan Park <ryanjpark03@gmail.com>
2025-07-27 15:24:53 -0500

    trajectory analysis

--- exp/Utils.jl
+++ exp/Utils.jl
@@ -6,6 +6,7 @@ using LinearAlgebra
 using BlockArrays
 using Infiltrator
 using RobustBeliefGame
+using Printf
 
 
 
@@ -16,7 +17,6 @@ function effect_of_nature(rh_file_ids; save_to_file=false)
     end
 
     for file_id in rh_file_ids
-        println("Analyzing file: $file_id.jld2")
         filepath = "exp/hockey/outputs/$file_id.jld2"
         if !isfile(filepath)
             println("File not found: $filepath")
@@ -25,32 +25,76 @@ function effect_of_nature(rh_file_ids; save_to_file=false)
 
         @load filepath gt_state_history all_observations goal_position solution_history
         
-        analysis_text = "Analysis for $file_id:\n"
+        analysis_text = "============================================================\n"
+        analysis_text *= @sprintf("ANALYSIS FOR: %s.jld2\n", file_id)
+        analysis_text *= "============================================================\n\n"
 
         for t in 1:length(solution_history)
-            non_robust_traj = solution_history[t][1]
-            robust_traj = solution_history[t][2]
+            non_robust_traj = solution_history[t][1][1]
+            robust_traj = solution_history[t][2][1]
+            nature_us_history = [solution_history[t][2][2][k][Block(3)] for k in 1:length(solution_history[t][2][2])]
+            non_robust_planned_us_history = [solution_history[t][1][2][k][Block(1):Block(2)] for k in 1:length(solution_history[t][1][2])]
+            robust_planned_us_history = [solution_history[t][2][2][k][Block(1):Block(2)] for k in 1:length(solution_history[t][2][2])]
             
             # 1. Absolute L2 norm difference between trajectories
-            traj_diff = sum(norm(means(non_robust_traj[k]) - means(robust_traj[k])) for k in eachindex(robust_traj))
-            analysis_text *= "  planned @t=$t: Trajectory Difference (L2 Norm): $traj_diff\n"
-
-            # 2. Average L2 norm difference per unit control cost of nature
-            nature_control_cost = norm(nature_us_history[t])^2
-            diff_per_cost = nature_control_cost > 1e-9 ? traj_diff / nature_control_cost : traj_diff
-            analysis_text *= "    - Diff per Nature's Control Cost: $diff_per_cost\n"
-            analysis_text *= "    - Nature's Control Cost: $nature_control_cost\n"
-
-            # 3. Difference in robust and non-robust actions
-            non_robust_us = planned_us_history[t][1]
-            robust_us = planned_us_history[t][2]
+            traj_diff = sum(norm(means(robust_traj[k])[Block(1):Block(2)] - means(robust_traj[k])[Block(3):Block(4)]) for k in eachindex(robust_traj))
             
-            # We only need to look at the first action, as this is what is executed
-            attacker_action_diff = norm(non_robust_us[1][Block(1)] - robust_us[1][Block(1)])
-            defender_action_diff = norm(non_robust_us[1][Block(2)] - robust_us[1][Block(2)])
+            # 2. Average L2 norm difference per unit control cost of nature
+            nature_control_cost = sum(
+                2_000*dot(nature_us_history[k], nature_us_history[k]) 
+                for k in eachindex(nature_us_history)
+                )
+            diff_per_cost = nature_control_cost > 1e-9 ? traj_diff / nature_control_cost : traj_diff
 
-            analysis_text *= "    - Attacker Action Difference (accel, steer): ($(-attacker_action_diff[1]), $(-attacker_action_diff[2]))\n"
-            analysis_text *= "    - Defender Action Difference (accel, steer): ($(-defender_action_diff[1]), $(-defender_action_diff[2]))\n"
+            # 3. Control costs for players
+            attacker_control_cost = sum(
+                4 * dot(robust_planned_us_history[k][1:2], robust_planned_us_history[k][1:2])
+                for k in eachindex(robust_planned_us_history)
+            )
+            defender_control_cost = sum(
+                2 * dot(robust_planned_us_history[k][3:4], robust_planned_us_history[k][3:4])
+                for k in eachindex(robust_planned_us_history)
+            )
+            attacker_diff_per_cost = attacker_control_cost > 1e-9 ? traj_diff / attacker_control_cost : traj_diff
+            defender_diff_per_cost = defender_control_cost > 1e-9 ? traj_diff / defender_control_cost : traj_diff
+
+            # 4. Difference in robust and non-robust actions
+            attacker_action_diff = (
+                sum(
+                    norm(non_robust_planned_us_history[t][1] - robust_planned_us_history[t][1])
+                    for k in eachindex(non_robust_planned_us_history[t])
+                ),
+                sum(
+                    norm(non_robust_planned_us_history[t][2] - robust_planned_us_history[t][2])
+                    for k in eachindex(non_robust_planned_us_history[t])
+                )
+            )
+            defender_action_diff = (
+                sum(
+                    norm(non_robust_planned_us_history[t][3] - robust_planned_us_history[t][3])
+                    for k in eachindex(non_robust_planned_us_history[t])
+                ),
+                sum(
+                    norm(non_robust_planned_us_history[t][4] - robust_planned_us_history[t][4])
+                    for k in eachindex(non_robust_planned_us_history[t])
+                )
+            )
+
+            analysis_text *= @sprintf("----------------- Timestep t=%d -----------------\n", t)
+            analysis_text *= "Trajectory Analysis:\n"
+            analysis_text *= @sprintf("  - Trajectory Difference    : %.5f\n", traj_diff)
+            analysis_text *= @sprintf("  - Nature's Control Cost    : %.5f\n", nature_control_cost)
+            analysis_text *= @sprintf("  - Attacker's Control Cost  : %.5f\n", attacker_control_cost)
+            analysis_text *= @sprintf("  - Defender's Control Cost  : %.5f\n\n", defender_control_cost)
+            analysis_text *= @sprintf("  - Nature's Diff per Cost   : %.5f\n", diff_per_cost)
+            analysis_text *= @sprintf("  - Attacker's Diff per Cost : %.5f\n", attacker_diff_per_cost)
+            analysis_text *= @sprintf("  - Defender's Diff per Cost : %.5f\n", defender_diff_per_cost)
+            analysis_text *= "\n"
+            analysis_text *= "Action Difference (Non-Robust vs. Robust):\n"
+            analysis_text *= @sprintf("  - Attacker (accel, steer)  : %.5f, %.5f\n", attacker_action_diff[1], attacker_action_diff[2])
+            analysis_text *= @sprintf("  - Defender (accel, steer)  : %.5f, %.5f\n", defender_action_diff[1], defender_action_diff[2])
+            analysis_text *= "\n"
+
         end
 
         println(analysis_text)
